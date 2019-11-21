9f4392aff75bf56363834269afc7fc5151a663d8
Ernest Man <ernestman@emsys9.local>
2019-11-21 13:39:19 +0000

    add object id for cd_group_expertui_v2

--- key-vault.tf
+++ key-vault.tf
@@ -6,7 +6,7 @@ module "key_vault" {
   tenant_id = "${var.tenant_id}"
   object_id = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
+  product_group_object_id = "7a7c6518-2381-408b-940c-7b9bd0256d9a" # dcd_group_expertui_v2
   common_tags = "${local.tags}"
   managed_identity_object_id  = "${var.managed_identity_object_id}"
 }
