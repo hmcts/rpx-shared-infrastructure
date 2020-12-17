bba6d1f2db09ebd9bfbf21acecc4bfc9f77578b7
ernestman28 <ernest_man28@hotmail.com>
2020-12-17 11:43:26 +0000

    Update TF & Azure RM

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
 locals {
-  tags = "${merge(var.common_tags,
+  tags = merge(var.common_tags,
     map("Team Contact", "${var.team_contact}")
-    )}"
+    )
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
-  location = "${var.location}"
-
-  tags = "${local.tags}"
+  location = var.location
+  tags = local.tags
 }
 
 provider "azurerm" {
-  version = "1.44.0"
+  feature {}
 }
