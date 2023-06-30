15bc9f2769fdcb2dea699f3cdf7c7dc2d2b46f5a
Hannah Smith <hannah.smith16@justice.gov.uk>
2023-06-30 10:38:47 +0100

    create new managed identity in correct subscription

--- key-vault.tf
+++ key-vault.tf
@@ -1,14 +1,15 @@
 module "key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name = "${var.product}-${var.env}"
-  product = var.product
-  env = var.env
-  tenant_id = var.tenant_id
-  object_id = var.jenkins_AAD_objectId
-  resource_group_name = azurerm_resource_group.rg.name
-  product_group_object_id = "7a7c6518-2381-408b-940c-7b9bd0256d9a" # dcd_group_expertui_v2
-  common_tags = local.tags
-  managed_identity_object_id  = var.managed_identity_object_id
+  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                       = "${var.product}-${var.env}"
+  product                    = var.product
+  env                        = var.env
+  tenant_id                  = var.tenant_id
+  object_id                  = var.jenkins_AAD_objectId
+  resource_group_name        = azurerm_resource_group.rg.name
+  product_group_object_id    = "7a7c6518-2381-408b-940c-7b9bd0256d9a" # dcd_group_expertui_v2
+  common_tags                = local.tags
+  managed_identity_object_id = var.managed_identity_object_id
+  create_managed_identity    = true
 }
 
 output "vaultName" {
