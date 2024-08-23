393cccba450d2d09b6749aacc479ab714346b46c
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2024-08-23 06:41:46 +0000

    Updating Terraform Formatting

--- key-vault.tf
+++ key-vault.tf
@@ -1,14 +1,14 @@
 module "key_vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                       = "${var.product}-${var.env}"
-  product                    = var.product
-  env                        = var.env
-  tenant_id                  = var.tenant_id
-  object_id                  = var.jenkins_AAD_objectId
-  resource_group_name        = azurerm_resource_group.rg.name
-  product_group_object_id    = "7a7c6518-2381-408b-940c-7b9bd0256d9a" # dcd_group_expertui_v2
-  common_tags                = local.tags
-  create_managed_identity    = true
+  source                               = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                                 = "${var.product}-${var.env}"
+  product                              = var.product
+  env                                  = var.env
+  tenant_id                            = var.tenant_id
+  object_id                            = var.jenkins_AAD_objectId
+  resource_group_name                  = azurerm_resource_group.rg.name
+  product_group_object_id              = "7a7c6518-2381-408b-940c-7b9bd0256d9a" # dcd_group_expertui_v2
+  common_tags                          = local.tags
+  create_managed_identity              = true
   additional_managed_identities_access = var.additional_managed_identities_access
 }
 
